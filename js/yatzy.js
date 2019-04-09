1bb0028ae9e52e86609a3d42d19071afac37495c
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-04-10 00:42:34 +0300

    Added opponent functionality.

--- js/yatzy.js
+++ js/yatzy.js
@@ -2,15 +2,17 @@ new Vue({
     el: '#game',
     data: {
         gameStarted: false,
-        timesThrown: 0,
-        throw: 0
+        timesThrow: 10,
+        playerThrown: 0,
+        opponentThrown: 0,
+        opponentPoints: 0,
+        playerPoints: 0,
+        playGoing: true
     },
     methods: {
-        rollDice: function() {
-            var dice = document.getElementById('dice');
-            var number = Math.floor(Math.random() * 5);
-            this.throw = number;
-            console.log("Value of the latest throw: " + ( 1+ this.throw));
+        enemyRollDice: function(argument) {
+            var dice = document.getElementById(argument);
+            var number = Math.floor(Math.random() * 6);
             switch (number) {
                 case 0:
                     dice.src = "graphics/dice_1.png";
@@ -33,7 +35,66 @@ new Vue({
                 default:
                     break;
             }
-            this.timesThrown += 1;
+            this.opponentThrown = number + 1;
+            console.log("Your dice: " + this.playerThrown + " Opponent's dice: " + this.opponentThrown);
+            this.addScore(this.playerThrown, this.opponentThrown);
+        },
+        rollDice: function(argument) {
+            if (this.playGoing) {
+                var dice = document.getElementById(argument);
+                var number = Math.floor(Math.random() * 6);
+                switch (number) {
+                    case 0:
+                        dice.src = "graphics/dice_1.png";
+                        break;
+                    case 1:
+                        dice.src = "graphics/dice_2.png";
+                        break;
+                    case 2:
+                        dice.src = "graphics/dice_3.png";
+                        break;
+                    case 3:
+                        dice.src = "graphics/dice_4.png";
+                        break;
+                    case 4:
+                        dice.src = "graphics/dice_5.png";
+                        break;
+                    case 5:
+                        dice.src = "graphics/dice_6.png";
+                        break;
+                    default:
+                        break;
+                }
+                this.playerThrown = number + 1;
+                this.timesThrow -= 1;
+                this.enemyRollDice('dice-enemy');
+                if (this.playGoing == true && this.timesThrow == 0) {
+                    this.playGoing = false;
+                    if (this.timesThrow <= 0) {
+                        this.findWinner(this.playerPoints, this.opponentPoints);
+                    }
+                }
+            }
+        },
+        addScore: function(pThrow, oThrow) {
+            if (pThrow > oThrow) {
+                this.playerPoints += 1;
+            } else if (oThrow > pThrow) {
+                this.opponentPoints += 1;
+            } else if (oThrow == pThrow) {
+                console.log('Even throws!');
+            }
+        },
+        findWinner: function(pPoints, oPoints) {
+            var sentence = ' ';
+            if (pPoints > oPoints) {
+                sentence += "Congratulations! You scored better than computer!";
+            } else if (oPoints > pPoints) {
+                sentence += "Computer won this round.";
+            } else if (oPoints == pPoints) {
+                sentence += "Game is even.";
+            }
+            alert(sentence);
         }
     },
     computed: {
